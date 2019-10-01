23d32e382319a9687432ff8264a7c7cfab9fbf82
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-01 15:48:33 +0100

    Removed repetition and changed var

--- directory.rb
+++ directory.rb
@@ -1,18 +1,25 @@
-student_count = 11
 #print students here
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# print students by array pos
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#print total students here
-print "Overall, we have #{student_count} great students"
+
+students.each do |student|
+    puts student
+end
+
+# total students here
+puts "Overall, we have #{students.count} great students"
 
