3a1fe3801b51632c0b1f0b75ae77821800d05a57
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-02 15:15:21 +0100

    Question 8.5

--- directory.rb
+++ directory.rb
@@ -1,15 +1,18 @@
 def input_students
-    puts "Please enter the names of the students"
+    puts "Please enter the names of the students, followed by cohort, then age"
     puts "To finish, just hit return twice"
     # create an empty array
     students = []
     # get the first name
     name = gets.chomp
+    # puts "Enter student's cohort"
     cohort = gets.chomp.to_sym
+    # puts "Enter student's age"
+    age = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do  
       # add the student hash to the array
-      students << {name: name, cohort: cohort}
+      students << {name: name, cohort: cohort, age: age}
       if students.count < 2
         puts "Only 1 student here"
       else 
@@ -18,6 +21,8 @@ def input_students
       # get another name from the user
       name = gets.chomp
       cohort = gets.chomp.to_sym
+      age = gets.chomp
+
     end
     # return the array of students
     students
@@ -29,18 +34,11 @@ def print_header
     puts "-------------"
   end
 
-#   def print(students, letter)
-#     students.select { |student| student[:name][0] == letter && student[:name].length < 12}
-#     .each_with_index do |student, index|  
-    #   puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-#     end
-#   end
-
   def print(students, letter)
     index = 0
      while index < students.count do
         if (students[index][:name].start_with? letter) && (students[index][:name].length < 12)
-        puts "#{index+1} - #{students[index][:name]}, #{students[index][:cohort]}"
+        puts "#{index+1} - #{students[index][:name]}, #{students[index][:cohort]}, #{students[index][:age]} "
      end
      index +=1
     end
