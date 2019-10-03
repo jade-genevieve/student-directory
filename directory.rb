d0ba357a06e655e7e889babc5edb1b06c26ec042
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-03 16:48:10 +0100

    Interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
 def input_students
   puts "Please enter the names of the students, followed by cohort, then age"
   puts "To finish, just hit return twice"
@@ -38,12 +64,12 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print(students, letter)
+def print(students)
   index = 0
   while index < students.count
-    if (students[index][:name].start_with? letter) && (students[index][:name].length < 12)
+    # if (students[index][:name].start_with? letter) && (students[index][:name].length < 12)
       puts "#{index + 1}. #{students[index][:name]}, #{students[index][:cohort]}, #{students[index][:age]} ".center(50)
-    end
+    # end
     index += 1
   end
 end
@@ -63,8 +89,4 @@ def print_cohort(students)
   students.sort_by { |student| student[:cohort] }.map { |student| puts"#{student[:name]}, #{student[:cohort]}, #{student[:age]}".center(50) }
 end
 
-students = input_students
-print_header
-print(students, "f")
-print_footer(students)
-print_cohort(students)
+interactive_menu
